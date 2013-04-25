97a51510541ee10c1ab3f478799023ff4e2f0c4b
Mark Ryall <mark@ryall.name>
2013-04-25 18:37:26 +1000

    added a lot of link commands

--- coffee/markryall.github.io.coffee
+++ coffee/markryall.github.io.coffee
@@ -1,12 +1,26 @@
 $ ->
-  commands = (command, term) ->
-    switch command
-      when 'help'
-        term.echo "available commands are help"
-      else
-        term.echo "unknown command"
+  say = (term, message) ->
+    term.echo message
     return
+
+  open = (url) ->
+    window.open url
+    return
+
+  commands = {
+    twitter: -> open 'http://twitter.com/markryall',
+    facebook: -> open 'http://facebook.com/mark.ryall',
+    github: -> open 'http://github.com/markryall',
+    linkedin: -> open 'http://linkedin.com/in/markryall',
+    flickr: -> open 'http://flickr.com/photos/markryall',
+    aboutme: -> open 'http://about.me/markryall',
+    lastfm: -> open 'http://last.fm/user/mryall'
+  }
+
+  help = 'available commands are: '
+  help += "\n\t#{key}" for key of commands
+  commands['help'] = -> say this, help
 
   $('body').terminal commands,
     greetings: 'welcome',
-    onBlur: -> false
+    onBlur: -> false,
--- js/markryall.github.io.js
+++ js/markryall.github.io.js
@@ -1,15 +1,42 @@
 (function() {
   $(function() {
-    var commands;
+    var commands, help, key, open, say;
 
-    commands = function(command, term) {
-      switch (command) {
-        case 'help':
-          term.echo("available commands are help");
-          break;
-        default:
-          term.echo("unknown command");
+    say = function(term, message) {
+      term.echo(message);
+    };
+    open = function(url) {
+      window.open(url);
+    };
+    commands = {
+      twitter: function() {
+        return open('http://twitter.com/markryall');
+      },
+      facebook: function() {
+        return open('http://facebook.com/mark.ryall');
+      },
+      github: function() {
+        return open('http://github.com/markryall');
+      },
+      linkedin: function() {
+        return open('http://linkedin.com/in/markryall');
+      },
+      flickr: function() {
+        return open('http://flickr.com/photos/markryall');
+      },
+      aboutme: function() {
+        return open('http://about.me/markryall');
+      },
+      lastfm: function() {
+        return open('http://last.fm/user/mryall');
       }
+    };
+    help = 'available commands are: ';
+    for (key in commands) {
+      help += "\n\t" + key;
+    }
+    commands['help'] = function() {
+      return say(this, help);
     };
     return $('body').terminal(commands, {
       greetings: 'welcome',
