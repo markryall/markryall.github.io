44bccdf7ed9b384202e71eb88c07be3ae825c75e
Mark Ryall <mark@ryall.name>
2013-05-01 08:43:12 +1000

    added websh as mobile view

---
+++ mobile/js/my.js
@@ -0,0 +1,21 @@
+$(function() {
+  $("#inputfield").focus();
+
+  $("#inputfield").keyup(function(e) {
+    switch(e.keyCode) {
+      case 13:
+        $('#history').append("<div>&gt; <span class=\"i\">"+$("#inputfield").val()+"</span></div>");
+        $("#inputfield").val("");
+        $("#inputfield").focus();
+        break;
+      case 38:
+        // up
+        break;
+      case 40:
+        // down
+        break;
+      default:
+        // alert(e.keyCode);
+    }
+  });
+});
